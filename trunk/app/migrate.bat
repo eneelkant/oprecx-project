4c2130b9a6e6077cb7a2c918d77e62e9e2496bfb
febri.aquila@gmail.com <febri.aquila@gmail.com@5c89ab9f-c1d4-a7e7-3f30-f65f5f431efb>
2013-03-22 08:36:39 +0000

    benerin migrate.bat

--- trunk/app/migrate.bat
+++ trunk/app/migrate.bat
@@ -2,8 +2,9 @@
 echo Database Migrate
 echo .
 echo Ketik perintah kemudian tekan enter
-echo pilihan : up | down | redo
+echo "pilihan : up | down | redo"
 set /p inp="> "
+set PHP_COMMAND=C:\xampp\php\php.exe
 yiic.bat migrate %inp%
 echo .
 echo Tekan sembarang tombol untuk keluar
