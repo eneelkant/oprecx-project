2eff4dbc0c7d88b421237419c4e2c32fc902e987
abiehafshin@gmail.com <abiehafshin@gmail.com@5c89ab9f-c1d4-a7e7-3f30-f65f5f431efb>
2013-03-22 09:31:18 +0000

    another commit

---
+++ trunk/app/modules/register/models/InfoForm.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of InfoForm
+ *
+ * @author abie
+ */
+class InfoForm extends CFormModel {
+    //put your code here
+}
+
+?>
