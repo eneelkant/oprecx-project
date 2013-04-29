5a80ca74530ad920cad012629bb35f7ed3ea9a40
syahidah.izza@gmail.com <syahidah.izza@gmail.com@5c89ab9f-c1d4-a7e7-3f30-f65f5f431efb>
2013-04-29 04:09:53 +0000

    field pendaftaran

--- trunk/Admin/field_spesifik.php
+++ trunk/Admin/field_spesifik.php
@@ -1,44 +1,46 @@
-<html>
+<html>  
 <head>  
-    <meta charset="UTF-8">
-    <title>Oprecx</title>
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.3.2/themes/default/easyui.css">
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.3.2/themes/icon.css">
-    <link rel="stylesheet" type="text/css" href="../demo.css">
-    <script type="text/javascript" src="jquery-easyui-1.3.2/jquery-1.8.0.min.js"></script>
+    <meta charset="UTF-8">  
+    <title>Oprecx</title>  
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.3.2/themes/default/easyui.css">  
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.3.2/themes/icon.css">  
+    <link rel="stylesheet" type="text/css" href="../demo.css">  
+    <script type="text/javascript" src="jquery-easyui-1.3.2/jquery-1.8.0.min.js"></script>  
     <script type="text/javascript" src="jquery-easyui-1.3.2/jquery.easyui.min.js"></script>
-</head>
-<body style="font-family: Calibri; margin-left: 150px; margin-top: 50px">  
-    <div class="demo-info">
-        <div class="demo-tip icon-tip"></div>
-    </div>
+</head>  
+<body style="font-family: Calibri; margin-left: 150px; margin-top: 50px">
+    <div class="demo-info">  
+        <div class="demo-tip icon-tip"></div>  
+    </div>  
     <div style="margin-left:50px 0;"></div>  
     <div class="easyui-layout" style="width:1000px;height:585px;border:none;">  
         <div data-options="region:'north'" style="height:75px;border:none;" ><img src="../images/logo.png" /></div>  
-        <div data-options="region:'south',split:true" style="height:50px;"></div>
+        <div data-options="region:'south',split:true" style="height:50px;"></div>    
         <div data-options="region:'west',split:true" title="Menu" style="width:230px;padding:10px;background-color:#DAECF5">
-            <table style="margin:5px; font-size: 23px">
+            <h1 style="line-height: 2px"> Admin </h1>
+            <h3> Nama Organisasi </h3>
+            <table style="font-size: 25px; margin: 5px; padding: 5px;">
                 <tr><td><a href ="index.php">Pengaturan</a></td></tr>
                 <tr><td><a href ="divisi.php">Divisi/Biro</a></td></tr>
-                <tr><td><a href ="field.php">Form Pendaftaran</a></td></tr>
+                <tr><td><a href ="field_general.php">Form Pendaftaran</a></td></tr>
                 <tr><td>Slot Wawancara</td></tr>
             </table>
-        </div>
-        <div data-options="region:'center',title:'Divisi/Biro',iconCls:'icon-ok'">
+        </div>  
+        <div data-options="region:'center',title:'Divisi/Biro',iconCls:'icon-ok'">  
             <div style="margin:10px 10;">
-      
-    <table id="dg" title="" class="easyui-datagrid" style="width:700px;height:250px"  
+    
+    <table id="dg" class="easyui-datagrid" style="width:700px;height:250px"  
             url="get_users.php"  
             toolbar="#toolbar" pagination="true"  
             rownumbers="true" fitColumns="true" singleSelect="true">  
-                    <thead>
-                    <tr>
-                <th field="nama" width="100">Nama</th>  
-                <th field="PJ" width="100">Nama PJ</th>  
-                <th field="kuota" width="50">Kuota</th>   
-                    </tr>
+        <thead>  
+            <tr>  
+                <th field="nama" width="100" >Nama</th>  
+                <th field="PJ" width="100">Tipe</th>
+                <th field="PJ" width="100">Wajib di Isi</th>
+            </tr>  
         </thead>  
-                </table>
+    </table>  
     <div id="toolbar">  
         <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newUser()">New</a>  
         <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editUser()">Edit</a>  
@@ -47,19 +49,17 @@
       
     <div id="dlg" class="easyui-dialog" style="width:400px;height:280px;padding:10px 20px"  
             closed="true" buttons="#dlg-buttons">  
-        <div class="ftitle">User Information</div>  
+        <div class="ftitle">Information</div>  
         <form id="fm" method="post" novalidate>  
             <div class="fitem">  
                 <label>Nama:</label>  
                 <input name="firstname" class="easyui-validatebox" required="true">  
             </div>  
             <div class="fitem">  
-                <label>Nama PJ:</label>  
-                <input name="lastname" class="easyui-validatebox" required="true">  
-            </div>  
-            <div class="fitem">  
-                <label>Kuota:</label>  
-                <input name="phone">  
+                <label>Tipe :</label>
+                <input name="tipe" class="easyui-validatebox" >
+                <!-- ke input gak ya datanya?? -->
+                <!--<input name="lastname" class="easyui-validatebox" required="true">-->  
             </div>  
         </form>  
     </div>  
@@ -142,5 +142,5 @@
             width:80px;  
         }  
     </style>  
-</body>
+</body>  
 </html>  
